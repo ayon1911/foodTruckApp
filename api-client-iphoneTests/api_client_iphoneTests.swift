91f074c97f7166555eef588debd21935e84ef56e
ayon1911 <khaledrahman-ayon@thomass-air.fritz.box>
2017-01-21 10:59:50 +0100

    Initial Commit

---
+++ api-client-iphoneTests/api_client_iphoneTests.swift
@@ -0,0 +1,36 @@
+//
+//  api_client_iphoneTests.swift
+//  api-client-iphoneTests
+//
+//  Created by Khaled Rahman-Ayon on 21/01/17.
+//  Copyright © 2017 Khaled Rahman-Ayon. All rights reserved.
+//
+
+import XCTest
+@testable import api_client_iphone
+
+class api_client_iphoneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
